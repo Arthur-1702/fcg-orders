3689e1da9ebaa5ecd972d57f40ee827912ffa2e4
Arthur <arthur-1702@hotmail.com>
2026-01-09 18:33:05 -0300

    Add HealthController to provide API health status endpoint

---
+++ API/Controllers/HealthController.cs
@@ -0,0 +1,23 @@
+﻿using API.Models;
+using Application.DTO.Order;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FCG.FiapCloudGames.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        /// <summary>
+        /// returns the current health status of the api.
+        /// </summary>
+        /// <returns>No content</returns>
+        [ProducesResponseType(typeof(OrderResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
+        [HttpGet(Name = "Health")]
+        public IActionResult Health()
+        {
+            return Ok();
+        }
+    }
+}
